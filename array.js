2671a836a31453ae902bac9ab24ce71acf05789e
Henrique Delacth <58345851+Delacth@users.noreply.github.com>
2023-10-10 22:32:24 +0100

    Update array.js

--- array.js
+++ array.js
@@ -15,14 +15,22 @@ for (let index = 0; index < animal.length; index++) {
 
 
     let computer = [
-        'HP',
-        'Lenovo',
+        {
+         name: 'HP',
+         quantidade : 22
+        },
+        {
+          name: 'Lenovo',
+          quantidade: 30
+        },
         {
             name : 'Samsung',
-            quantidade : 20,
-            
+            quantidade : 20            
         },
-        'Dell'
+        {
+            name: 'DELL',
+            quantidade : 15
+        }
     ]
 
     console.log(computer[2].quantidade)
